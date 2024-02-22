bf555ecc22aaa5e62a4dc4bd1cac2003d31226bc
charliean0501 <charliean4@gmail.com>
2024-02-22 10:55:28 -0600

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Charlie An on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Charlie An on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
